64d9214616bacfe0e3a8809da0a00413e130ba88
Литвинович Владислав <vladkefir2003@mail.ru>
2025-01-09 15:26:54 +0300

    task4 answer fix

--- src/task4/task4.java
+++ src/task4/task4.java
@@ -8,7 +8,7 @@ import java.util.OptionalInt;
 
 public class task4 {
     public static void main(String[] args) throws IOException {
-        BufferedReader circle = new BufferedReader(new FileReader("src\\task4\\numbs.txt"));
+        BufferedReader circle = new BufferedReader(new FileReader(args[0]));
         ArrayList<Integer> arr = new ArrayList<>();
         String buff = circle.readLine();
         while (buff != null) {
@@ -21,21 +21,24 @@ public class task4 {
     public static int letsMakeTheSame(ArrayList<Integer> arr) {
         int res = 0;
         int average = arr.stream().mapToInt(Integer::intValue).sum() / arr.size();
-        long countLess = arr.stream().filter(number -> number < average).count();
-        long countMore = arr.stream().filter(number -> number > average).count();
-        if (countMore == countLess) {
+        //System.out.println(average);
+        long countLess = arr.stream().filter(number -> number < average + 0.5).count();
+        //System.out.println(countLess);
+        long countMore = arr.stream().filter(number -> number > average + 0.5).count();
+        //System.out.println(countMore);
+        if (countMore == countLess ) {
             for (Integer integer : arr) {
                 res += Math.abs(integer - average);
             }
             return res;
         } else if (countMore > countLess) {
-            OptionalInt buff = arr.stream().filter(number -> number > average).mapToInt(Integer::intValue).min();
+            OptionalInt buff = arr.stream().filter(number -> number >= average).mapToInt(Integer::intValue).min();
             for (Integer integer : arr) {
                 res += Math.abs(integer - buff.orElse(0));
             }
             return res;
         } else {
-            OptionalInt buff = arr.stream().filter(number -> number < average).mapToInt(Integer::intValue).min();
+            OptionalInt buff = arr.stream().filter(number -> number <= average).mapToInt(Integer::intValue).max();
             for (Integer integer : arr) {
                 res += Math.abs(integer - buff.orElse(0));
             }
